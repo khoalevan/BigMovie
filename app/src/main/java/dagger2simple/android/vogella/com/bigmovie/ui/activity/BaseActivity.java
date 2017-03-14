a1e047b378a6c3d49aff9e550da556aef40af2c1
Khoa Le Van <khoalv1@fsoft.com.vn>
2017-03-15 01:38:34 +0700

    first commit

---
+++ app/src/main/java/dagger2simple/android/vogella/com/bigmovie/ui/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package dagger2simple.android.vogella.com.bigmovie.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by khoalevan on 3/15/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/dagger2simple/android/vogella/com/bigmovie/ui/activity/BrowseMoviesActivity.java
@@ -0,0 +1,8 @@
+package dagger2simple.android.vogella.com.bigmovie.ui.activity;
+
+/**
+ * Created by khoalevan on 3/15/17.
+ */
+
+public class BrowseMoviesActivity extends BaseActivity {
+}
