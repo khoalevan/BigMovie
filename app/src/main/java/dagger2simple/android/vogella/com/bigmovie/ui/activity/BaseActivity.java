99ffba02ceffbd7cc23a22911f2e233e4f6fbc87
Khoa Le Van <khoalv1@fsoft.com.vn>
2017-03-16 01:13:55 +0700

    update code 1:13 16/3

--- app/src/main/java/dagger2simple/android/vogella/com/bigmovie/ui/activity/BaseActivity.java
+++ app/src/main/java/dagger2simple/android/vogella/com/bigmovie/ui/activity/BaseActivity.java
@@ -1,10 +1,17 @@
 package dagger2simple.android.vogella.com.bigmovie.ui.activity;
 
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import dagger2simple.android.vogella.com.bigmovie.R;
 
 /**
  * Created by khoalevan on 3/15/17.
  */
 
 public class BaseActivity extends AppCompatActivity {
+    @Nullable @Bind(R.id.toolbar) Toolbar mToolbar;
 }
